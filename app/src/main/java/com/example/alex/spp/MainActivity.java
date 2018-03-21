2109ab6b2f14f413f748680a37b5ddfc4c4f7a9d
Oleksandr Shvets <pascal36@yandex.com>
2018-03-21 13:00:47 +0100

    icons on action bar

--- app/src/main/java/com/example/alex/spp/MainActivity.java
+++ app/src/main/java/com/example/alex/spp/MainActivity.java
@@ -15,6 +15,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -89,6 +91,27 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.item1:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
