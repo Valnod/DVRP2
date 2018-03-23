27437175d7f410de0429e3270727ccea1bd8c958
Oleksandr Shvets <pascal36@yandex.com>
2018-03-23 09:53:54 +0100

    settings activity fragments

---
+++ app/src/main/java/com/example/alex/spp/PagerAdapter.java
@@ -0,0 +1,10 @@
+package com.example.alex.spp;
+
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by Alex on 22/03/2018.
+ */
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+}
